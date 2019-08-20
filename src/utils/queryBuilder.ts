d5af9890f751814156dc6e083cb9abda53e2e6bc
Michal <preterer@gmail.com>
2019-08-20 22:28:32 +0200

    extended query builder

---
+++ src/utils/queryBuilder.ts
@@ -0,0 +1,28 @@
+import { SelectQueryBuilder } from "typeorm";
+import { CoreEntity } from "../entities/core.entity";
+
+/**
+ * Extended query builder
+ *
+ * @export
+ * @class QueryBuilder
+ * @extends {SelectQueryBuilder<Entity>}
+ * @template Entity
+ */
+export class QueryBuilder<Entity extends CoreEntity> extends SelectQueryBuilder<Entity> {
+  /**
+   * Adds a where clause with LIKE
+   *
+   * @param {string} field
+   * @param {string} value
+   * @returns {QueryBuilder<Entity>}
+   * @memberof QueryBuilder
+   */
+  andLike(field: string, value: string): QueryBuilder<Entity> {
+    return this.andWhere(`${this.tableName}.${field} LIKE :like${field}`, { [`like${field}`]: value });
+  }
+
+  constructor(queryBuilder: SelectQueryBuilder<Entity>, public tableName: string) {
+    super(queryBuilder);
+  }
+}
