b905899373c093e4fefe5aa3620e5c899ba003ae
Michal <preterer@gmail.com>
2019-08-21 19:39:38 +0200

    user tests

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -1,28 +1,108 @@
 import bcrypt from "bcrypt";
-import * as TypeORM from "typeorm";
+import { Container } from "typedi";
 
-import { mockDB, mockData, userData } from "./testUtils";
-import { UserRepository } from "../src/repositories/user.repository";
+import { UserService } from "../src/services/user.service";
+
+import { mockDB, mockData, userData, clearData } from "./testUtils";
+import { User } from "../src/entities/user.entity";
 
 describe("User", function() {
-  let userRepository: UserRepository, userId: number, roleId: number;
+  let userService: UserService, userId: number, roleId: number, amounts: number;
 
   beforeAll(async function() {
     await mockDB();
-    userRepository = TypeORM.getCustomRepository(UserRepository);
+    userService = Container.get(UserService);
   });
 
   beforeEach(async function() {
     const data = await mockData();
     userId = data.userId;
     roleId = data.roleId;
+    amounts = data.amounts;
+  });
+
+  afterEach(async function() {
+    await clearData();
+  });
+
+  describe("get", function() {
+    it("should get an user", async function() {
+      const user = await userService.get(userId);
+      expect(user).not.toBeUndefined();
+      expect(user instanceof User).toBeTruthy();
+    });
+
+    it("should not get a non existing user", async function() {
+      await expect(userService.get(userId + 1)).rejects.toThrow();
+    });
+  });
+
+  describe("list", function() {
+    it("should find all users", async function() {
+      const results = await userService.list();
+      expect(results.list.length).toEqual(amounts);
+      expect(results.count).toEqual(amounts);
+    });
+
+    it("should find an user with login matching search", async function() {
+      const user = await userService.get(userId);
+      const searches = [
+        user.login.slice(0, 4),
+        user.login.slice(4, 8),
+        user.login.slice(user.login.length - 4, user.login.length - 1)
+      ];
+      for (const search of searches) {
+        const results = await userService.list({ search });
+        expect(results.list.length).toBeGreaterThan(0);
+        expect(results.count).toBeGreaterThan(0);
+      }
+    });
+
+    it("should not find user with login that doesn't match search", async function() {
+      const results = await userService.list({ search: "8a89eseaej9a8sjeaskle" });
+      expect(results.list.length).toBe(0);
+      expect(results.count).toBe(0);
+    });
+  });
+
+  describe("add", function() {
+    it("should add a new user", async function() {
+      const user = await userService.add({ login: "test", password: "test" });
+      expect(user).not.toBeUndefined();
+      expect(await userService.get(user.id)).not.toBeUndefined();
+    });
+  });
+
+  describe("update", function() {
+    it("should update user", async function() {
+      const login = "test12345";
+      await userService.update(userId, { login });
+      const user = await userService.get(userId);
+      expect(user.login).toBe(login);
+    });
+
+    it("should not update a non existing user", async function() {
+      const login = "test12345";
+      await expect(userService.update(userId + 1, { login })).rejects.toThrow();
+    });
+  });
+
+  describe("delete", function() {
+    it("should delete an user", async function() {
+      await userService.delete(userId);
+      await expect(userService.get(userId)).rejects.toThrow();
+    });
+
+    it("should not delete a non existing user", async function() {
+      await expect(userService.delete(userId + 1)).rejects.toThrow();
+    });
   });
 
   describe("hashPassword", function() {
     it("should has password on create", async function() {
       const newUserData = userData(userId + 1);
-      const newUserId = await userRepository.saveModel(newUserData).then(user => user.id);
-      const newUser = await userRepository.findOne(newUserId);
+      const newUserId = await userService.add(newUserData).then(user => user.id);
+      const newUser = await userService.get(newUserId);
       expect(newUser.password).not.toBe(newUserData.password);
       const areMatching = await bcrypt.compare(newUserData.password, newUser.password);
       expect(areMatching).toBeTruthy();
@@ -30,63 +110,47 @@ describe("User", function() {
 
     it("should hash password on update", async function() {
       const newPassword = "newPassword";
-      const user = await userRepository.findOne(userId);
+      const user = await userService.get(userId);
       user.password = newPassword;
-      await userRepository.saveModel(user);
-      const updatedUser = await userRepository.findOne(userId);
+      await userService.update(user.id, user);
+      const updatedUser = await userService.get(userId);
       expect(updatedUser.password).not.toBe(newPassword);
       const areMatching = await bcrypt.compare(newPassword, updatedUser.password);
       expect(areMatching).toBeTruthy();
     });
   });
 
-  describe("permissionAdd", function() {
-    it("should add a new permission to user", async function() {
-      // const name = "PERMISSION";
-      // await userRepository.permissionAdd(userId, { name });
-      // const user = await userRepository.findOne(userId);
-      // const userPermissions = await user.permissions;
-      // expect(userPermissions.length).toBeGreaterThan(0);
-      // expect(!!userPermissions.find(permission => permission.name === name)).toBeTruthy();
-    });
-
-    it("should not add a permission to user that doesn't exist", async function() {
-      // const name = "PERMISSION";
-      // await expect(userRepository.permissionAdd(userId + 1, { name })).rejects.toThrow();
-    });
-  });
-
-  describe("permissionRemove", function() {
-    it("should remove a permission which was previously assigned to user", function() {});
-
-    it("should not remove a permission which was not assigned to user", function() {});
-  });
-
   describe("roleAdd", function() {
-    it("should assign a role to user", function() {});
+    it("should assign a role to user", async function() {
+      await userService.roleAdd(userId, roleId);
+      const roles = await userService.get(userId).then(user => user.roles);
+      expect(roles.length).toBeGreaterThan(0);
+      expect(roles.some(role => role.id === roleId)).toBeTruthy();
+    });
 
-    it("should not assign a role to user that doesn't exist", function() {});
+    it("should not assign a role to user that doesn't exist", async function() {
+      await expect(userService.roleAdd(userId + 1, roleId)).rejects.toThrow();
+    });
 
-    it("should not assign a role that doesn't exist", function() {});
+    it("should not assign a role that doesn't exist", async function() {
+      await expect(userService.roleAdd(userId, roleId + 1)).rejects.toThrow();
+    });
   });
 
   describe("roleRemove", function() {
-    it("should unassign a role from user", function() {});
-
-    it("should not unassign a role from user that doesn't exist", function() {});
-
-    it("should not assign a role which is not previously assigned", function() {});
-  });
-
-  describe("filter", function() {
-    it("should find an user with login matching search", async function() {
-      const results = await userRepository.filter({ search: "user" }).getMany();
-      expect(results.length).toBeGreaterThan(0);
+    it("should unassign a role from user", async function() {
+      await userService.roleAdd(userId, roleId);
+      await userService.roleRemove(userId, roleId);
+      const roles = await userService.get(userId).then(user => user.roles);
+      expect(roles.some(role => role.id === roleId)).toBeFalsy();
     });
 
-    it("should not find user with login that doesn't match search", async function() {
-      const results = await userRepository.filter({ search: "8a89eseaej9a8sjeaskle" }).getMany();
-      expect(results.length).toBe(0);
+    it("should not unassign a role from user that doesn't exist", async function() {
+      await expect(userService.roleRemove(userId + 1, roleId)).rejects.toThrow();
+    });
+
+    it("should not assign a role which is not previously assigned", async function() {
+      await expect(userService.roleRemove(userId, roleId)).rejects.toThrow();
     });
   });
 });
